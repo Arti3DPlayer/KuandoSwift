72aa9d6cd9f16e92c37412223e15a73e754f5558
Eric Betts <bettse@fastmail.fm>
2015-06-19 19:28:59 -0700

    Initial Commit

---
+++ KuandoSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  KuandoSwift
+//
+//  Created by Eric Betts on 6/19/15.
+//  Copyright © 2015 Eric Betts. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+

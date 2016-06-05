273f07eb0cdc7444ec2d9ebe2fa3d29f308f3aaa
Eric Betts <bettse@fastmail.fm>
2016-06-05 11:47:04 -0700

    Update selectoc syntax

--- KuandoSwift/main.swift
+++ KuandoSwift/main.swift
@@ -10,7 +10,7 @@ import Foundation
 import AppKit
 
 let busylight = BusyLight.singleton
-var daemon = NSThread(target: busylight, selector:"initUsb", object: nil)
+var daemon = NSThread(target: busylight, selector:#selector(BusyLight.initUsb), object: nil)
 
 daemon.start()
 NSRunLoop.currentRunLoop().run()
